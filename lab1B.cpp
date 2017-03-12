ae2ab2fb2d1e53885908f82485c81bef323845e2
leo199483 <leo199483@gmail.com>
2017-03-12 23:50:18 +0800

    11:50 ver.02

--- lab1B.cpp
+++ lab1B.cpp
@@ -1,12 +1,24 @@
+/*********************
+HW1
+Leo Chien
+2017/03/12
+*********************/
+
 #include <iostream>
+
 using namespace std;
+
 int main(){
-int n;
-cin>>n;
-step2:cout<<n<<endl;
-if(n==1) return 0;
-if(n%2==1) n=3*n+1;
-else n=n/2;
-goto step2;
-return 0;
+	int n;	
+
+	cin>>n;
+	step2:cout<<n<<endl;
+
+	if(n==1) return 0;
+	if(n%2==1) n=3*n+1;	
+	else n=n/2;
+	
+	goto step2;	
+
+	return 0;
 }
